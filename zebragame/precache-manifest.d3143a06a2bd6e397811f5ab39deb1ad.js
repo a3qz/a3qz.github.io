09e5fe864fd6110a419e6bbb8514fa14b8d1dd0c
a3qz <rmichalec14@gmail.com>
2020-05-09 21:20:26 -0700

    zebra

---
+++ zebragame/precache-manifest.d3143a06a2bd6e397811f5ab39deb1ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b4cb9e0e135d680f63e351148cb424",
+    "url": "/zebragame/index.html"
+  },
+  {
+    "revision": "118e3931d78694524448",
+    "url": "/zebragame/static/css/main.ab60916a.chunk.css"
+  },
+  {
+    "revision": "4d85efb769d05dce6ccb",
+    "url": "/zebragame/static/js/2.fe32ef40.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/zebragame/static/js/2.fe32ef40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "118e3931d78694524448",
+    "url": "/zebragame/static/js/main.cedd8a50.chunk.js"
+  },
+  {
+    "revision": "86ad97158752bfeefc39",
+    "url": "/zebragame/static/js/runtime-main.eaaf2b5c.js"
+  }
+]);
